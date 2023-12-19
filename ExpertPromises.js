9c1cccb116abecc9199c23fcd9569c269ae36d0f
Tayyab-103 <tayyabhameed103@gmail.com>
2023-12-19 19:27:27 +0500

    Reject and Resolve

---
+++ ExpertPromises.js
@@ -0,0 +1,14 @@
+// Promises:
+
+let promise = new Promise(function(resolve, reject){
+    alert("I am an alert in promise")
+}) 
+
+
+console.log("Hello")
+setTimeout(function(){
+    console.log("Hello in SetTimeOut with 2 Seconds")
+}, 2000)
+
+
+console.log("My name is " +  "Tayyab")
--- Pomises.js
+++ Pomises.js
@@ -1,63 +1,63 @@
 //Web Api Rules:
-function printHello(){
-    console.log('Hello')
-  }
-  setTimeout(printHello, 1000)
+// function printHello(){
+//     console.log('Hello')
+//   }
+//   setTimeout(printHello, 1000)
   
-  console.log("my First")
+//   console.log("my First")
 
 
-function printHello(){
-  console.log("Hello")
-}
-function blockFor1Sec(){ // blocks in the javascript thread for 1 sec
+// function printHello(){
+//   console.log("Hello")
+// }
+// function blockFor1Sec(){ // blocks in the javascript thread for 1 sec
 
-}
+// }
 
-setTimeout(printHello,  0 );
+// setTimeout(printHello,  0 );
 
-blockFor1Sec()
-console.log("My First")
+// blockFor1Sec()
+// console.log("My First")
 
 
-function delayedGreet() {
+// function delayedGreet() {
   
-  setTimeout(()=> {
-    console.log('Welcome!');
-  }, 3000);
-}
+//   setTimeout(()=> {
+//     console.log('Welcome!');
+//   }, 3000);
+// }
 
 
-// Uncomment the following line to check your work!
-delayedGreet(); 
+// // Uncomment the following line to check your work!
+// delayedGreet(); 
 
 /* CHALLENGE 3 */
 
-function helloGoodbye() {
-  console.log("hello")
-  setTimeout(()=>{
-    console.log("Good bye")
-  },2000)
-  // ADD CODE HERE
-}
-// Uncomment the following line to check your work!
-helloGoodbye();
+// function helloGoodbye() {
+//   console.log("hello")
+//   setTimeout(()=>{
+//     console.log("Good bye")
+//   },2000)
+//   // ADD CODE HERE
+// }
+// // Uncomment the following line to check your work!
+// helloGoodbye();
 
 
 /* CHALLENGE 4 */
-function brokenRecord() {
-  const intervalId = setInterval(() => {
-    console.log('hi again');
-  }, 1000); // 1000 milliseconds (1 second)
+// function brokenRecord() {
+//   const intervalId = setInterval(() => {
+//     console.log('hi again');
+//   }, 1000); // 1000 milliseconds (1 second)
 
-  // Stop after 10 seconds (adjust the time as needed)
-  setTimeout(() => {
-    clearInterval(intervalId);
-    console.log('Stopped the brokenRecord function.');
-  }, 10000); // 10000 milliseconds (10 seconds)
-}
+//   // Stop after 10 seconds (adjust the time as needed)
+//   setTimeout(() => {
+//     clearInterval(intervalId);
+//     console.log('Stopped the brokenRecord function.');
+//   }, 10000); // 10000 milliseconds (10 seconds)
+// }
 
-// Example usage
-brokenRecord();
+// // Example usage
+// brokenRecord();
 
 
