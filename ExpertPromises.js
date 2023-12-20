ef4f5c155bfd8c69b7d4bf502270665d0509de2e
Tayyab-103 <tayyabhameed103@gmail.com>
2023-12-20 19:52:43 +0500

    Promises Complete

--- ExpertPromises.js
+++ ExpertPromises.js
@@ -1,32 +1,37 @@
-// Promises: 
-let promise = new Promise(function(resolve, reject){
-    alert("I am an alert in promise")
-}) 
-console.log("Hello")
-setTimeout(function(){
-    console.log("Hello in SetTimeOut with 2 Seconds")
-}, 2000)
+// Promises:
+// let promise = new Promise(function(resolve, reject){
+//     alert("I am an alert in promise")
+// })
+// console.log("Hello")
+// setTimeout(function(){
+//     console.log("Hello in SetTimeOut with 2 Seconds")
+// }, 2000)
 
+// console.log("My name is " +  "Tayyab")
 
-console.log("My name is " +  "Tayyab")
+// function display(data){
+//     console.log(data)
+// }
 
-
-
-function display(data){
-    console.log(data)
-}
-
-// const futureData = fetch('https://twitter.com/will/tweets/1')
+// const futureData = fetch('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8')
 // futureData.then(display)
 
 // console.log("My First")
 
-// function display(data) {
-//     console.log(data);
-// }
+function display(data) {
+  console.log(data);
+}
+function printHello() {
+  console.log("Hello");
+}
+function blockFor300ms() {}
+setTimeout(printHello, 2000);
 
-// const futureData = fetch('https://twitter.com/will/tweets/1')
-//     .then(response => response.json()) // Parse the JSON data
-//     .then(data => display(data)) // Display the parsed data
-
-// console.log("My First");
+const futureData = fetch(
+  "https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8"
+)
+.then((response) => response.json())
+futureData.then(display)
+// .then((data) => display(data));  
+blockFor300ms();
+console.log("Me First");
