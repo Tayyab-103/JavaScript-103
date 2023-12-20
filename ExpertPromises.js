64ef86a47e052475f5a76100d48f25f7a502753d
Tayyab-103 <tayyabhameed103@gmail.com>
2023-12-20 15:10:26 +0500

    Show Alert using Promise

--- ExpertPromises.js
+++ ExpertPromises.js
@@ -1,10 +1,7 @@
-// Promises:
-
+// Promises: 
 let promise = new Promise(function(resolve, reject){
     alert("I am an alert in promise")
 }) 
-
-
 console.log("Hello")
 setTimeout(function(){
     console.log("Hello in SetTimeOut with 2 Seconds")
@@ -12,3 +9,24 @@ setTimeout(function(){
 
 
 console.log("My name is " +  "Tayyab")
+
+
+
+function display(data){
+    console.log(data)
+}
+
+// const futureData = fetch('https://twitter.com/will/tweets/1')
+// futureData.then(display)
+
+// console.log("My First")
+
+// function display(data) {
+//     console.log(data);
+// }
+
+// const futureData = fetch('https://twitter.com/will/tweets/1')
+//     .then(response => response.json()) // Parse the JSON data
+//     .then(data => display(data)) // Display the parsed data
+
+// console.log("My First");
