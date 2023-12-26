f275e5314ea054c3aa747e4401f375870ff217b6
Tayyab-103 <tayyabhameed103@gmail.com>
2023-12-26 18:52:46 +0500

    Prtotype Chain Review

--- ClassPrototypes.js
+++ ClassPrototypes.js
@@ -53,10 +53,45 @@ const userFunctionStroe = {
   increment: function () {
     this.score++;
   },
-  login: function(){
-    console.log("Logged in")
-  }
+  login: function () {
+    console.log("Logged in");
+  },
 };
-const user5 = createUser("Kami", 33)
-user5.increment()
-console.log(user5)
+const user5 = createUser("Kami", 33);
+// user5.increment()
+// user5.hasOwnProperty('score')
+// console.log(user5.hasOwnProperty('name'))
+console.log(user5);
+
+const user6 = new createUser("jon", 66);
+console.log(user6);
+
+// Prototype Chian Review
+function multiplyBy2(num) {
+  return num * 2;
+}
+
+multiplyBy2.stored = 5;
+multiplyBy2(3);
+
+multiplyBy2.stored;
+multiplyBy2.prototype;
+
+console.log(multiplyBy2.stored);
+console.log(multiplyBy2.prototype);
+
+// // New Kewword
+// function moreUserCreator(name, score) {
+//   this.name = name;
+//   this.score = score;
+// }
+
+// moreUserCreator.prototype.increment = function () {
+//   this.score++;
+// };
+// moreUserCreator.prototype.login = function () {
+//   console.log("login");
+// };
+
+// const user7 = new moreUserCreator("Eva", 9);
+// console.log(user7)
