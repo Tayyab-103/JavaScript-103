ed3e4636850969316995f8ad94c1d683123a772e
Tayyab-103 <tayyabhameed103@gmail.com>
2024-01-14 23:18:50 +0500

    Deep Copy

--- ShallowDeep.js
+++ ShallowDeep.js
@@ -3,3 +3,24 @@ const originalArray = [1,2,{a:3}]
 const shallowCopy = [...originalArray]
 
 console.log(originalArray)
+
+function deepCopy(obj) {
+    // Convert the object to a JSON string
+    var jsonString = JSON.stringify(obj);
+  
+    // Parse the JSON string to create a new object
+    var copy = JSON.parse(jsonString);
+  
+    return copy;
+  }
+  
+  // Example usage
+  var originalObject = { a: 1, b: { c: 2, d: [3, 4] } };
+  var deepCopyObject = deepCopy(originalObject);
+  
+  // Now, modifying the originalObject won't affect deepCopyObject
+  originalObject.b.d[0] = 99;
+  
+  console.log(originalObject);        // { a: 1, b: { c: 2, d: [99, 4] } }
+  console.log(deepCopyObject);        // { a: 1, b: { c: 2, d: [3, 4] } }
+  
