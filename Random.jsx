9835b4a61c9875376e96832d525a9f8915023966
Tayyab-103 <tayyabhameed103@gmail.com>
2024-08-01 23:27:11 +0500

    Random Number Component

---
+++ Random.jsx
@@ -0,0 +1,10 @@
+
+const Random = () => {
+
+    let number = Math.random() *100;
+  return (
+    <div style={{'background-color': "red"}}>Random Number is: {Math.round(number)}</div>
+  )
+}
+
+export default Random
